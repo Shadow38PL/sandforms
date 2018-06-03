8111ee0f096ab02c5b7e509103a58261840801ef
Shadow38PL <bjuraszewski@heseya.com>
2018-06-03 13:39:29 +0200

    Nieskończona wersja z częściową obsługą językow

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once 'render.php';
+
+    echo render('pages/index', ['hahaha' => 'haHAAAAAAA']);
+?>
---
+++ render.php
@@ -0,0 +1,75 @@
+<?php
+    $localizations = [
+        'en' => [
+            'hello_world' => 'Hello world!'
+        ],
+        'pl' =>  [
+            'hello_world' => 'Witaj świecie!'
+        ]
+    ];
+
+    $defaultLanguage = 'en';
+
+    function isLocalized ($text, $lang) {
+        global $localizations;
+
+        if (array_key_exists($lang, $localizations))
+            if (array_key_exists($text, $localizations[$lang]))
+                return true;
+
+        return false;
+    }
+
+    function localize ($text) {
+        global $localizations;
+        global $defaultLanguage;
+        $lang = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE'])[0];
+
+        if (isLocalized($text, $lang))
+            return $localizations[$lang][$text];
+        
+        $lang = explode('-', $lang)[0];
+
+        if (isLocalized($text, $lang))
+            return $localizations[$lang][$text];
+
+        $lang = $defaultLanguage;
+
+        if (isLocalized($text, $lang))
+            return $localizations[$lang][$text];
+        
+        return "### Error: No lacalization for text '".$text."' ###";
+    }
+
+    function loadFile($path) {
+        $path = preg_match('/\..+$/', $path) ? $path : $path.'.html';
+        $file = file_get_contents($path);
+        return $file ? $file : "### Error: No such file '".$path."' ###";
+    }
+
+    function inject ($text, $open, $close, $params) {
+        $text = preg_replace_callback($open.'.*'.$close, function ($match) use ($open, $close, $params) {
+            $match = preg_replace('('.$open.'\s*|\s*'.$close.')', '', $match[0]);
+            if ($match[0] == '$') {
+                if (array_key_exists(substr($match, 1), $params))
+                    $match = $params[substr($match, 1)];
+                else
+                    $match = "### Error: No such param '".substr($match, 1)."' ###";
+            } else if ($match[0] == '@') {
+                $match = loadFile(substr($match, 1));
+            } else if ($match[0] == '#') {
+                $match = localize(substr($match, 1));
+            }
+            
+            return $match;
+        }, $text);
+        return $text;
+    }
+    
+    function render ($path, $params) {
+        $text = loadFile($path);
+        $text = preg_replace('/<!--[^!].*-->/', '', $text);
+        $text = inject($text, '{{', '}}', $params);
+        return $text;
+    }
+?>
