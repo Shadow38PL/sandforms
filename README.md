fa6c09c5a472d97f2750fe4eeef1a41575852065
Bartosz Juraszewski <bjuraszewski@gmail.com>
2018-06-09 15:06:47 +0200

    Znaczniki osadzone w betonie, @ odwołuje się do root katalogu witryny

---
+++ README.md
@@ -0,0 +1,10 @@
+### Znaczniki zewnętrzne ###
+{*  *} - Wkleja zawartość jako kod HTML
+{{  }} - Wkleja zawartość jako zwykły tekst
+
+### Znaczniki wewnętrzne ### - Są prefiksami do zawartości zanczników zewnętrznych
+Brak znacznika - Oznacza zmienną przekazywaną z backendu - np. {{ zmienna }} //Zawartość zmiennej
+$ - Oznacza zmienną językową która zostanie przetłumaczona na język użytkownika zgodnie z plikami lokalizacyjnymi - {{ $hello_world }} //Witaj świecie!
+@ - Oznacza import z pliku, jeśli nie poda się rozszerzenia, domyślnie wybierze .html - np. {* @templates/banner *} //Kod html z pliku templates/banner.html
+
+UWAGA : @ odwołuje się do root katalogu, jeśli chcemy uzyskać dostęp do plików pod nim należy zastosować w ścieżce '../' czyli zejście poziom niżej
--- render.php
+++ render.php
@@ -43,23 +43,23 @@
 
     function loadFile($path) {
         $path = preg_match('/\..+$/', $path) ? $path : $path.'.html';
-        $file = file_get_contents($path);
-        return $file ? $file : "### Error: No such file '".$path."' ###";
+        $fullpath = $_SERVER["DOCUMENT_ROOT"] . '/' . $path;
+        return file_exists($fullpath) ? file_get_contents($fullpath) : "### Error: No such file '".$path."' ###";
     }
 
     function inject ($text, $open, $close, $params, $escape) {
-        //$open.'[^('.$close.')]*'.$close
-        $text = preg_replace_callback('~'.$open.'[^\*}]*'.$close.'~', function ($match) use ($open, $close, $params, $escape) {
+        $text = preg_replace_callback('/'.$open.'[^\*}]*'.$close.'/', function ($match) use ($open, $close, $params, $escape) {
             $match = preg_replace('('.$open.'\s*|\s*'.$close.')', '', $match[0]);
-            if ($match[0] == '$') {
-                if (array_key_exists(substr($match, 1), $params))
-                    $match = $params[substr($match, 1)];
-                else
-                    $match = "### Error: No such param '".substr($match, 1)."' ###";
-            } else if ($match[0] == '@') {
+            
+            if ($match[0] == '@') {
                 $match = loadFile(substr($match, 1));
-            } else if ($match[0] == '#') {
+            } else if ($match[0] == '$') {
                 $match = localize(substr($match, 1));
+            } else {
+                if (array_key_exists($match, $params))
+                    $match = $params[$match];
+                else
+                    $match = "### Error: No such param '".$match."' ###";
             }
             
             return $escape ? htmlspecialchars($match) : $match;
